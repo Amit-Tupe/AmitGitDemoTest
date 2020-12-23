d43f64c838d5273d9165f8430145c3f95c7d622d
AMITRAJE <AMITRAJE@192.168.1.204>
2020-12-23 10:23:53 +0530

    Change By Amit Add New Method Check

--- SeleniumAdvGITHUB_9/src/Demo/Hello.java
+++ SeleniumAdvGITHUB_9/src/Demo/Hello.java
@@ -2,6 +2,11 @@ package Demo;
 
 public class Hello {
 	
+	public void Check()
+	{
+		System.out.println("Check");
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Git");
